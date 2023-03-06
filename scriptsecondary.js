500ace37585740bfb64f87841b1fe7088c2b5a46
Stanislav Stoyanov <stani30000@gmail.com>
2023-03-06 16:06:33 +0000

    Update and rename script.js to scriptsecondary.js

--- scriptsecondary.js
+++ scriptsecondary.js
@@ -7,16 +7,19 @@ function fn1(){
    var grammar = document.querySelector('input[name="grammar"]:checked').value;
    var clarity = document.querySelector('input[name="clarity"]:checked').value;
    // send email
-   var templateParams = {
+   var data = {
       fluency: fluency,
       grammar: grammar,
       clarity: clarity
    };
-   emailjs.send('service_i8nk7oo', 'template1', templateParams)
-      .then(function(response) {
-         console.log(fluency,grammar,clarity,templateParams)
-         console.log('SUCCESS!', response.status, response.text);
-      }, function(error) {
-         console.log('FAILED...', error);
-      });
+   //emailjs.send('service_i8nk7oo', 'template1', data)
+   //   .then(function(response) {
+   //      console.log("sent:",data)
+   //      console.log('SUCCESS!', response.status, response.text);
+   //   }, function(error) {
+   //      console.log('FAILED...', error);
+   //   });
+   
+   // test log
+   console.log(data);
    }
