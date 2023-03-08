08f14d0ae585953c68f4a8af460748512037b63a
Stanislav Stoyanov <stani30000@gmail.com>
2023-03-08 13:14:11 +0000

    Update scriptsecondary.js

--- scriptsecondary.js
+++ scriptsecondary.js
@@ -1,5 +1,25 @@
+
 const button = document.getElementById("submit");
 button.addEventListener("click", fn1);
+
+examples = [];
+fetch('https://bananko7.github.io/cattcleaned.txt')
+//fetch('file:///E:/hons%20project/site/secondarypage.html')
+   .then(response => response.text())
+   .then((data) => {
+      //console.log(data);
+      examples = data.replace("\n", ",").split(",");
+   });
+
+
+//console.log(client.responseText);
+//client.send();
+
+function getrandomitem(examples){
+   const index = Math.floor(Math.random()*examples.length);
+   const item = examples[index];
+   return item;
+}
 
 function fn1(){
    // get form data
@@ -7,19 +27,21 @@ function fn1(){
    var grammar = document.querySelector('input[name="grammar"]:checked').value;
    var clarity = document.querySelector('input[name="clarity"]:checked').value;
    // send email
-   var data = {
+   var templateParams = {
       fluency: fluency,
       grammar: grammar,
       clarity: clarity
    };
-   //emailjs.send('service_i8nk7oo', 'template1', data)
+   //emailjs.send('service_i8nk7oo', 'template1', templateParams)
    //   .then(function(response) {
-   //      console.log("sent:",data)
+   //      console.log(fluency,grammar,clarity,templateParams)
    //      console.log('SUCCESS!', response.status, response.text);
    //   }, function(error) {
    //      console.log('FAILED...', error);
    //   });
-   
-   // test log
-   console.log(data);
-   }
+   //loadnewexample(); 
+   var newtext = getrandomitem(examples);
+   //var newtext = "<span style=\"background-color: #ffff00\">amsterdam airport schipol</span> serves <span style=\"background-color: #ffff00\">amsterdam</span>";
+   console.log(newtext);
+   document.getElementById('text_article').innerHTML = newtext;
+}
